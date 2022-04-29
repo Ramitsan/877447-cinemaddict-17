a2584fc832506ee2ff5b212757d6562d1f98efc3
Ramitsan <ramitsan@gmail.com>
2022-04-29 22:49:40 +0300

    Отрисовывает сортировку

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import FiltersView from './view/filters-view.js';
 import FilmCardView from './view/film-card-view.js';
+import SortingView from './view/sorting-view';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 
 render(new FiltersView(), siteMainElement);
+render(new SortingView(), siteMainElement);
 render(new FilmCardView(), siteMainElement);
