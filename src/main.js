51823e55418cb387b626359207b49d9406627d75
Ramitsan <ramitsan@gmail.com>
2022-04-30 15:44:27 +0300

    Отрисовывает попап

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import UserProfileView from './view/user-profile-view.js';
 import FiltersView from './view/filters-view.js';
 import SortingView from './view/sorting-view.js';
 import FilmsCountView from './view/films-count-view.js';
-import {render} from './render.js';
+import PopupView from './view/popup-view.js';
+import {RenderPosition, render} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
 const headerElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const footerElement = document.querySelector('.footer');
 const footerStatisticsElement = document.querySelector('.footer__statistics');
 const boardPresenter = new BoardPresenter();
 
@@ -14,5 +16,6 @@ render(new UserProfileView(), headerElement);
 render(new FiltersView(), siteMainElement);
 render(new SortingView(), siteMainElement);
 render(new FilmsCountView(), footerStatisticsElement);
+render(new PopupView(), footerElement, RenderPosition.AFTEREND);
 
 boardPresenter.init(siteMainElement);
