e7d84b3e4108e25fa24dd20ef786278d9869334a
Ramitsan <ramitsan@gmail.com>
2022-04-30 18:20:35 +0300

    Исправляет отрисовку статистики в футере

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import UserProfileView from './view/user-profile-view.js';
 import FiltersView from './view/filters-view.js';
 import SortingView from './view/sorting-view.js';
-import FilmsCountView from './view/films-count-view.js';
+import FooterStatisticsView from './view/footer-statistics-view.js';
 import PopupView from './view/popup-view.js';
 import {RenderPosition, render} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
@@ -9,13 +9,12 @@ import BoardPresenter from './presenter/board-presenter.js';
 const headerElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const footerElement = document.querySelector('.footer');
-const footerStatisticsElement = document.querySelector('.footer__statistics');
 const boardPresenter = new BoardPresenter();
 
 render(new UserProfileView(), headerElement);
 render(new FiltersView(), siteMainElement);
 render(new SortingView(), siteMainElement);
-render(new FilmsCountView(), footerStatisticsElement);
+render(new FooterStatisticsView(), footerElement);
 render(new PopupView(), footerElement, RenderPosition.AFTEREND);
 
 boardPresenter.init(siteMainElement);
--- src/view/footer-statistics-view.js
+++ src/view/footer-statistics-view.js
@@ -1,10 +1,14 @@
 import { createElement } from '../render.js';
 
-const createFilmsCountTemplate = () => '<p>130 291 movies inside</p>';
+const createFooterStatisticsTemplate = () => (
+  `<section class="footer__statistics">
+    <p>130 291 movies inside</p>
+  </section>`
+);
 
-export default class FilmsCountView {
+export default class FooterStatisticsView {
   getTemplate() {
-    return createFilmsCountTemplate();
+    return createFooterStatisticsTemplate();
   }
 
   getElement() {
