314d83fcd47ce5ab634df1e4dc7d738526692204
Ramitsan <ramitsan@gmail.com>
2022-04-30 16:23:45 +0300

    Исправляет ошибки линтера

--- src/view/films-list-title-view.js
+++ src/view/films-list-title-view.js
@@ -1,6 +1,6 @@
 import { createElement } from '../render.js';
 
-const createFilmsListTitleTemplate = () => `<h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>`;
+const createFilmsListTitleTemplate = () => '<h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>';
 
 export default class FilmsListTitleView {
   getTemplate() {
--- src/view/films-list-view.js
+++ src/view/films-list-view.js
@@ -1,6 +1,6 @@
 import { createElement } from '../render.js';
 
-const createFilmsListTemplate = () => `<section class="films-list"></section>`;
+const createFilmsListTemplate = () => '<section class="films-list"></section>';
 
 export default class FilmsListView {
   getTemplate() {
