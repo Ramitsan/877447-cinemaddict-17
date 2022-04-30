bdd9a51a1325e916fb92175b34e7a80722152021
Ramitsan <ramitsan@gmail.com>
2022-04-30 14:58:56 +0300

    Отрисовывает профиль пользователя

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+import UserProfileView from './view/user-profile-view.js';
 import FiltersView from './view/filters-view.js';
 import SortingView from './view/sorting-view.js';
 import {render} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
+const headerElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const boardPresenter = new BoardPresenter();
 
+render(new UserProfileView(), headerElement);
 render(new FiltersView(), siteMainElement);
 render(new SortingView(), siteMainElement);
 
