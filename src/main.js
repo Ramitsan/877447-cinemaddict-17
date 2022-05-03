319a15f56815c30b2117dc2367d57e215a32c25d
Ramitsan <ramitsan@gmail.com>
2022-05-03 19:15:31 +0300

    Переносит отрисовку попапа из main.js в презентер и передает в него данные из модели

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import UserProfileView from './view/user-profile-view.js';
 import FiltersView from './view/filters-view.js';
 import SortingView from './view/sorting-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
-import PopupView from './view/popup-view.js';
-import {RenderPosition, render} from './render.js';
+import {render} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import CardsModel from './model/cards-model.js';
 
@@ -17,6 +16,5 @@ render(new UserProfileView(), headerElement);
 render(new FiltersView(), siteMainElement);
 render(new SortingView(), siteMainElement);
 render(new FooterStatisticsView(), footerElement);
-render(new PopupView(), footerElement, RenderPosition.AFTEREND);
 
 boardPresenter.init(siteMainElement, cardsModel);
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,8 +4,10 @@ import FilmsListTitleView from '../view/films-list-title-view.js';
 import FilmsListContainerView from '../view/films-list-container-view.js';
 import FilmCardView from '../view/film-card-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
-import { render } from '../render.js';
+import PopupView from '../view/popup-view.js';
+import {RenderPosition, render} from '../render.js';
 
+const footerElement = document.querySelector('.footer');
 const cardCount = 5;
 
 export default class BoardPresenter {
@@ -24,6 +26,7 @@ export default class BoardPresenter {
     render(this.FilmsListComponent, this.FilmsSectionComponent.getElement());
     render(new FilmsListTitleView(), this.FilmsListComponent.getElement());
     render(this.FilmsListContainerComponent, this.FilmsListComponent.getElement());
+    render(new PopupView(this.boardCards[0]), footerElement, RenderPosition.AFTEREND);
 
     for (let i = 0; i < this.boardCards.length; i++) {
       render(new FilmCardView(this.boardCards[i]), this.FilmsListContainerComponent.getElement());
