5babaccefdd1eda8e2c541cda7b5e7ef18f83f73
Ramitsan <ramitsan@gmail.com>
2022-06-08 00:33:17 +0300

    Добавляет модель для фильтров, рефакторит вью

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ import { generateCard } from './mock/card.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import CardsModel from './model/cards-model.js';
 import CommentsModel from './model/comments-model.js';
-import {generateFilter} from './mock/filter.js';
+import FilterModel from './model/filters-model.js';
 import { TOTAL_CARD_COUNT } from './const.js';
+
+const filters = [
+  {
+    type: 'all',
+    name: 'ALL',
+    count: 0,
+  },
+];
 
 const headerElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
@@ -15,6 +23,7 @@ const footerElement = document.querySelector('.footer');
 
 const cardsModel = new CardsModel();
 const commentsModel = new CommentsModel();
+const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter(siteMainElement, cardsModel, commentsModel);
 
 render(new UserProfileView(), headerElement);
@@ -24,6 +33,5 @@ for (let i = 0; i < TOTAL_CARD_COUNT; i++) {
   generateCard(cardsModel, commentsModel);
 }
 
-const filters = generateFilter(cardsModel.cards);
-render(new FiltersView(filters), siteMainElement);
+render(new FiltersView(filters, 'all'), siteMainElement);
 boardPresenter.init();
--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,18 +1,18 @@
 import AbstractView from '../framework/view/abstract-view.js';
 import { FilterType, FilterName } from '../const.js';
 
-const createFilterItemTemplate = (filter, isActive) => {
-  const { name, count } = filter;
+const createFilterItemTemplate = (filter, currentFilterType) => {
+  const { type, name, count } = filter;
   const filmsCount = name === FilterType.ALL ? '' : `<span class="main-navigation__item-count">${count}</span>`;
-  const isFilterActive = isActive ? 'main-navigation__item--active' : '';
+  const isFilterActive = currentFilterType ? 'main-navigation__item--active' : '';
 
   return (
     `<a href="#${name}" class="main-navigation__item ${isFilterActive}">${FilterName[name]} ${filmsCount}</a>`
   );
 };
 
-const createFiltersTemplate = (filterItems) => {
-  const filterItemsTemplate = filterItems.map((filter, index) => createFilterItemTemplate(filter, index === 0)).join('');
+const createFiltersTemplate = (filterItems, currentFilterType) => {
+  const filterItemsTemplate = filterItems.map((filter) => createFilterItemTemplate(filter, currentFilterType)).join('');
 
   return (
     `<nav class="main-navigation"> 
@@ -23,12 +23,25 @@ const createFiltersTemplate = (filterItems) => {
 
 export default class FiltersView extends AbstractView {
   #filters = null;
-  constructor(filters) {
+  #currentFilter = null;
+
+  constructor(filters, currentFilterType) {
     super();
     this.#filters = filters;
+    this.#currentFilter = currentFilterType;
   }
 
   get template() {
-    return createFiltersTemplate(this.#filters);
+    return createFiltersTemplate(this.#filters, this.#currentFilter);
   }
+
+  setFilterTypeChangeHandler = (callback) => {
+    this._callback.setFilterTypeChange = callback;
+    this.element.addEventListener('change', this.#filterTypeChangeHandler);
+  };
+
+  #filterTypeChangeHandler = (evt) => {
+    evt.preventDefault();
+    this._callback.filterTypeChange(evt.target.value);
+  };
 }
